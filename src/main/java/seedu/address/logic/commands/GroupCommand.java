5300f7e78099304b4b3e69ae1cb9b16012f42d98
Zhou-or-Joe <ZhouXuyan6@gmail.com>
2024-04-02 12:07:03 +0800

    Update GroupCommand and GroupCommandParser to incorporate more checks
    To ensure the basic functionality of the groupCommand. Although it still does not work as expected.

--- src/main/java/seedu/address/logic/commands/GroupCommand.java
+++ src/main/java/seedu/address/logic/commands/GroupCommand.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CATEGORY;
+
 import seedu.address.model.Model;
 
 /**
@@ -10,8 +13,25 @@ public class GroupCommand extends Command {
     public static final String TOO_MANY_CATEGORIES = "Please specify only 1 category";
     public static final String NO_INPUT = "Please specify a category";
 
+    /**
+     * Usage message for the group command.
+     */
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Groups contacts in the address book. "
+            + "Parameters: "
+            + PREFIX_CATEGORY + "CATEGORY\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_CATEGORY + "CLAN ";
+
     private String category;
+
+    /**
+     * Constructs a GroupCommand with the specified category.
+     *
+     * @param category The category by which contacts will be grouped. Must not be null.
+     * @throws NullPointerException if the specified category is null.
+     */
     public GroupCommand(String category) {
+        requireNonNull(category);
         this.category = category;
     }
 
@@ -21,6 +41,7 @@ public class GroupCommand extends Command {
      * @return success result
      */
     public CommandResult execute(Model model) {
+        requireNonNull(model);
         model.groupPerson(category);
         return new CommandResult("All persons have been grouped!");
     }
