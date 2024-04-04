e7d808921e13fcdf69ecde9b06e3ea77383a2c91
Hong Ray <119933763+HongRay@users.noreply.github.com>
2024-04-04 14:10:37 +0800

    Fix edit command
    fix bug for editing name and case sensitivity.

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -28,6 +28,7 @@ import seedu.address.model.tag.Tag;
  * Edits the details of an existing person in the address book.
  */
 public class EditCommand extends Command {
+    //testing pull request
     public static final String COMMAND_WORD = "edit";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
