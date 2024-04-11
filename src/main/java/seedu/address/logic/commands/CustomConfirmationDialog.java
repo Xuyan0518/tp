2c93665db6f40a5659f17a6e665609daaf75fe8a
Zhou-or-Joe <ZhouXuyan6@gmail.com>
2024-04-11 15:39:22 +0800

    Bug fixes
    Clear command not has a pop-up alert that asks the user to confirm his action. Users can now either use mouse to confirm action or use arrow key to navigate between the choices and use "enter" key to confirm selection.

--- src/main/java/seedu/address/logic/commands/CustomConfirmationDialog.java
+++ src/main/java/seedu/address/logic/commands/CustomConfirmationDialog.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -8,6 +9,8 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import javafx.event.ActionEvent;
 
 /**
  * A custom dialog for confirmation with OK and Cancel buttons.
@@ -20,16 +23,67 @@ public class CustomConfirmationDialog extends Dialog<ButtonType> {
      * @param title   The title of the dialog.
      * @param message The message to be displayed in the dialog.
      */
+//    public CustomConfirmationDialog(String title, String message) {
+//        setTitle(title);
+//        initStyle(StageStyle.UTILITY);
+//        setHeaderText(null);
+//        setContentText(message);
+//
+//        ButtonType okButton = new ButtonType("OK");
+//        ButtonType cancelButton = new ButtonType("Cancel");
+//
+//        getDialogPane().getButtonTypes().addAll(okButton, cancelButton);
+//
+//        getDialogPane().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+//            if (event.getCode() == KeyCode.ENTER) {
+//                Node focusedNode = getDialogPane().getScene().getFocusOwner();
+//                if (focusedNode instanceof Button) {
+//                    Button focusedButton = (Button) focusedNode;
+//                    if (focusedButton.getText().equals("OK")) {
+//                        setResult(ButtonType.OK);
+//                        hide();
+//                    } else if (focusedButton.getText().equals("Cancel")) {
+//                        setResult(ButtonType.CANCEL);
+//                        hide();
+//                    }
+//                }
+//            } else if (event.getCode() == KeyCode.ESCAPE) {
+//                setResult(ButtonType.CANCEL);
+//                hide();
+//            } else if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.DOWN) {
+//                getDialogPane().lookupButton(okButton).requestFocus();
+//            } else if (event.getCode() == KeyCode.LEFT || event.getCode() == KeyCode.UP) {
+//                getDialogPane().lookupButton(cancelButton).requestFocus();
+//            }
+//        });
+//
+//        // Set the default button to OK
+//        Stage stage = (Stage) getDialogPane().getScene().getWindow();
+//        stage.setOnShown(e -> getDialogPane().getScene().getRoot().requestFocus());
+//    }
+
     public CustomConfirmationDialog(String title, String message) {
         setTitle(title);
         initStyle(StageStyle.UTILITY);
         setHeaderText(null);
         setContentText(message);
 
-        ButtonType okButton = new ButtonType("OK");
-        ButtonType cancelButton = new ButtonType("Cancel");
+        ButtonType okButton = new ButtonType("OK", ButtonType.OK.getButtonData());
+        ButtonType cancelButton = new ButtonType("Cancel", ButtonType.CANCEL.getButtonData());
 
         getDialogPane().getButtonTypes().addAll(okButton, cancelButton);
+
+        EventHandler<ActionEvent> actionEventHandler = event -> {
+            ButtonType resultButtonType = ((Button) event.getSource()).getText().equals("OK") ? ButtonType.OK : ButtonType.CANCEL;
+            setResult(resultButtonType);
+            hide();
+        };
+
+        Button okBtn = (Button) getDialogPane().lookupButton(okButton);
+        okBtn.setOnAction(actionEventHandler);
+
+        Button cancelBtn = (Button) getDialogPane().lookupButton(cancelButton);
+        cancelBtn.setOnAction(actionEventHandler);
 
         getDialogPane().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
             if (event.getCode() == KeyCode.ENTER) {
