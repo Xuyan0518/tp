377e0ac7fe64a867c0323dc17387eb37c7da293e
Hong Ray <119933763+HongRay@users.noreply.github.com>
2024-04-12 01:57:28 +0800

    Improved Code Quality
    Improved code quality for my portion.
    
    Weird bug with Junit test for GroupTest.
    Edit:
    Fixed the above problem. It was due to the static person inside that addressbook being manipulated which caused the problem.
    
    Using the unused person in GroupTest solves this problem.

--- src/test/java/seedu/address/model/person/GroupTest.java
+++ src/test/java/seedu/address/model/person/GroupTest.java
@@ -3,7 +3,7 @@ package seedu.address.model.person;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBookGroup;
 
 import java.util.ArrayList;
 
@@ -14,24 +14,24 @@ import seedu.address.model.AddressBook;
 
 
 public class GroupTest {
-    private final AddressBook addressBook = getTypicalAddressBook();
+    private final AddressBook addressBook = getTypicalAddressBookGroup();
     private final Group group = new Group();
     @Test
-    public void group_nonEmptyListDifferentCategories_success() throws CommandException {
+    public void group_nonEmptyListDifferentCategoriesUsingTypicalPerson_success() throws CommandException {
         ArrayList<Person> persons = new ArrayList<>(addressBook.getPersonList());
         group.group(persons, "Clan");
         ArrayList<Person> groupedPersons = group.getGroupList();
 
-        assertEquals(7, groupedPersons.size());
+        assertEquals(4, groupedPersons.size());
     }
     @Test
-    public void group_nonEmptyListMissingCategory_groupedInNoCategory() throws CommandException {
+    public void group_nonEmptyListMissingCategoryUsingTypicalPerson_groupedInNoCategory() throws CommandException {
         ArrayList<Person> persons = new ArrayList<>(addressBook.getPersonList());
         group.group(persons, "Clan");
         ArrayList<Person> groupedPersons = group.getGroupList();
 
         // Assuming there's one person with a category and one without
-        assertEquals(7, groupedPersons.size());
+        assertEquals(4, groupedPersons.size());
     }
     @Test
     public void group_emptyList_throwsCommandException() {
@@ -40,13 +40,13 @@ public class GroupTest {
         assertThrows(CommandException.class, () -> group.group(persons, "Category"));
     }
     @Test
-    public void getGroupList_afterGrouping_returnsCorrectGroups() throws CommandException {
+    public void getGroupList_afterGroupingUsingTypicalPerson_returnsCorrectGroups() throws CommandException {
         ArrayList<Person> persons = new ArrayList<>(addressBook.getPersonList());
 
         group.group(persons, "Clan");
         ArrayList<Person> groupedPersons = group.getGroupList();
 
         assertNotNull(groupedPersons);
-        assertEquals(7, groupedPersons.size());
+        assertEquals(4, groupedPersons.size());
     }
 }
